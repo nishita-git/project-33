1fd9b50419e7770bdb6203db8597ee4b9bc9cb29
nishita-git <80521868+nishita-git@users.noreply.github.com>
2021-06-30 16:48:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+var bg1,bg2,bg3
+var maxsnow
+var snow=[]
+var rand
+function preload(){
+  bg1=loadImage("snow1.jpg")
+  bg2=loadImage("snow2.jpg")
+  bg3=loadImage("snow3.jpg")
+
+}
+function setup() {
+  createCanvas(800,400);
+  background(bg1);  
+
+  engine=Engine.create()
+  world=engine.world
+}
+
+function draw() {
+
+  Engine.update(engine)
+
+  if(frameCount%200===0){
+    rand=Math.round(random(1,3))  
+  }
+
+  if(rand===1){
+    background(bg1);  
+    fill("black");
+    textSize(25);
+    text("Happy Winters!",330,60);
+    }
+   else if(rand===2){
+      background(bg2); 
+      fill("yellow");
+      textSize(25);
+      text("It's snowing!",300,60);
+      }
+   else if(rand===3){
+        background(bg3);  
+        fill("red");
+        textSize(25);
+        text("Santa is on his way.",250,60);  
+        }
+    else{
+        background(bg1);  
+        }
+if(frameCount%5===0){
+  snow.push(new Snow(random(0,800),30,30))
+}
+for(var j=0;j<snow.length;j++){
+  snow[j].display();
+}
+  
+}
